59f1dda9b7002f30027ad6b63825116b17612317
Jfm73 <42632212+Jfm73@users.noreply.github.com>
2020-08-28 10:27:08 -0500

    deleting then adding in old files back to branch
    fidingout if i can add old files back to the branch

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  #sets main to index page    
-  root 'pages#main'    
-    
-  get 'pages/main'
-  get 'pages/hosting'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end

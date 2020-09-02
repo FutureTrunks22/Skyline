235ce47c5f912045fd301998710f3cbdd68d5b73
Jfm73 <42632212+Jfm73@users.noreply.github.com>
2020-09-02 18:05:57 -0500

    kinda updated 3 files here, left comments
    this meant to 3 seperate commits

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,15 @@
 class PagesController < ApplicationController
 def index
-    @contact = Home.new(params[:home])
+    @contact = home.new(params[:home])
   end
 
   def create
-    @contact = Home.new(params[:home])
+    @contact = home.new(params[:home])
     @contact.request = request
     respond_to do |format|
       if @contact.deliver
         # re-initialize Home object for cleared form
-        @contact = Home.new
+        @contact = home.new
         format.html { render 'index'}
         format.js   { flash.now[:success] = @message = "Thank you for your message. I'll get back to you soon!" }
       else
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   #sets main to index page    
-  root 'pages#hosting'    
+  #root 'pages#hosting'    
     
-  get 'pages/main'
-  get 'pages/hosting'
+  #get 'pages/main'
+  #get 'pages/hosting'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #added this root and resources in so it can initialize variables thats needed in the hosting.html file
+  root      'pages#index'
+  resources :pages, only: [:index, :new, :create]
 end

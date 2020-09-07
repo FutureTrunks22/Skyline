8b93d7eeaed1f4910e6c472263e2b2a251d483a6
Jfm73 <42632212+Jfm73@users.noreply.github.com>
2020-09-06 22:20:34 -0500

    Update production.rb
    im trying a new heroku  build named skykinehostingsitev2, maybe i did something wrong the first time

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,7 +110,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-config.action_mailer.default_url_options = { :host => 'skylinehostingsite.herokuapp.com' }  
+config.action_mailer.default_url_options = { :host => 'skylinehostingsitev2.herokuapp.com' }  
 config.action_mailer.delivery_method = :smtp  
 config.action_mailer.perform_deliveries = true  
 config.action_mailer.raise_delivery_errors = false  
@@ -118,9 +118,9 @@ config.action_mailer.default :charset => "utf-8"
 config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
-  domain:               'skylinehostingsite.herokuapp.com',
+  domain:               'skylinehostingsitev2.herokuapp.com',
   user_name:            ENV["GMAIL_EMAIL"],
   password:             ENV["GMAIL_PASSWORD"],
   authentication:       'plain',
-   }
+  enable_starttls_auto: true  }
 end

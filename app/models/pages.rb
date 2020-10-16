3bd3c12854866db525acafa07433a979d8f89a50
Jfm73 <42632212+Jfm73@users.noreply.github.com>
2020-10-15 23:28:42 -0500

    updates
    gemfile update
    created new label with temp place nam ehere name
    
    new attribute for label in pages model
    
    not sure why that pacman stackdump is here but im not gonna touch that

--- app/models/pages.rb
+++ app/models/pages.rb
@@ -4,6 +4,7 @@ class Pages < MailForm::Base
   attribute :name,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
+  attribute :place
   attribute :nickname,  :captcha  => true
 
   # Declare the e-mail headers. It accepts anything the mail method

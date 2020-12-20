c4105d05f5f3c61d0afac9eb2b0649340ca2bfed
Jfm73 <42632212+Jfm73@users.noreply.github.com>
2020-12-19 20:25:11 -0600

    updated contact form
    changed the contact form to match picture in the discord, also added some variable declarations in the pages.rb file that are used in _contact_form

--- app/models/pages.rb
+++ app/models/pages.rb
@@ -3,10 +3,12 @@
 class Pages < MailForm::Base
   attribute :name,      :validate => true
   attribute :Company,      :validate => true
+  attribute :Phone_Number,      :validate => true
+  attribute :service_interest,      :validate => true
   attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
   attribute :message
   attribute :place
-  attribute :nickname,  :captcha  => true
+  
 
   # Declare the e-mail headers. It accepts anything the mail method
   # in ActionMailer accepts.
